98bedfcf8f0ba324290d43e331c4b5038782f092
jazzdragon <climbinghobo@gmail.com>
2017-01-05 12:02:28 -0700

    added sql file

---
+++ schema.sql
@@ -0,0 +1,71 @@
+CREATE TYPE address AS (
+  street TEXT,
+  state CHAR(2),
+  country VARCHAR(50),
+  zip_code VARCHAR(10)
+);
+
+CREATE TABLE clients (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE,
+  time_zone VARCHAR(50),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(_id) NOT NULL,
+  UNIQUE(name, client_id)
+);
+
+CREATE TABLE inventory (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT,
+  sku VARCHAR(50),
+  category_id INT REFERENCES categories(_id),
+
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(_id) NOT NULL,
+  UNIQUE(sku, client_id)
+);
+
+CREATE TABLE users (
+  _id SERIAL PRIMARY KEY,
+  username VARCHAR(100) NOT NULL UNIQUE,
+  password CHAR(60),
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(_id) NOT NULL
+);
+
+CREATE TABLE customers (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL,
+  type VARCHAR(30),
+  user_id INT REFERENCES users(_id),
+  email VARCHAR(50) UNIQUE,
+  student_id VARCHAR(50) UNIQUE,
+  phone_number CHAR(10) UNIQUE CHECK(phone_number ~ '[0-9]{10}'),
+  address ADDRESS,
+  birth_date DATE,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(_id) NOT NULL
+);
+
+CREATE TABLE reservations (
+  _id SERIAL PRIMARY KEY,
+  inventory_id INT REFERENCES inventory(_id) NOT NULL,
+  customer_id INT REFERENCES users(_id) NOT NULL,
+  start_timestamp TIMESTAMPTZ NOT NULL,
+  end_timestamp TIMESTAMPTZ NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  client_id INT REFERENCES clients(_id) NOT NULL
+);
